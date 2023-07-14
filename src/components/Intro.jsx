6e2cf6a084438d8f0eeb7959ef94c70e72cb46c7
Dosu Roseline <dosuroseline@yahoo.com>
2023-07-14 14:11:59 +0100

    Added colors

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -12,16 +12,19 @@ function Intro() {
         style={{ backgroundImage: `url(${bg})` }}
       >
         <div className=" text-white text-start px-5 md:px-20">
-          <p className="py-4 flex gap-2">
+          <p className="py-4 flex gap-2 text-[#E5FBFF]">
             <img src={line} alt="" /> Hi, my name is
           </p>
-          <h1 className="text-[26px] md:text-[56px] lg:text-[66px]  font-normal  leading-none pb-4">
+          <h1 className="text-[26px] md:text-[56px] lg:text-[66px] text-[#CEF7FF]  font-normal  leading-none pb-4">
             Roseline Peace
           </h1>
-          <h1 className="text-[23px] md:text-[50px] lg:text-[66px] font-normal leading-none">
+          <h1 className="text-[23px] md:text-[50px] lg:text-[66px] text-[#E5FBFF] font-normal leading-none">
             I build things for the web
           </h1>
-          <p className="text-[16px] md:text-[24px] font-normal text-[#D9D9D9] w-[90%] py-8 md:w-[76%] lg:w-[49%] leading-none">
+          <p
+            className="text-[16px] md:text-[20px] font-normal text-[#D9D9D9] w-[90%] py-8 md:w-[76%] lg:w-[49%]"
+            style={{ fontFamily: 'Manrope, sans-serif' }}
+          >
             I am a passionate frontend developer with a keen eye for crafting
             beautiful and intuitive user experiences. With a solid foundation in
             HTML, CSS, and JavaScript, I strive to create pixel-perfect designs
